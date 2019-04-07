a21b5ed07606669a988e30bd1b924f5afb37f53c
Rama Ferguson <ferguson.rama@gmail.com>
2019-04-06 20:27:24 -0700

    created accounts.js and glob repository
    - added npm package glob
    - created accounts.js
      > will be used to store functions for parsing userdata, usernames, passwords etc

---
+++ accounts.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+/** function that should be run when user submits username on login.
+ * Parses all user json files in users directory for user with matching username
+ * @param {username} username string
+*/
+function checkUserNameLogIn(username) {
+
+    pass
+}
+
+module.exports = {
+    checkUserNameLogIn
+}
