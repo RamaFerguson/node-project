40750d4cb3d93e81ea5c57360ee1036f73409782
Rama Ferguson <ferguson.rama@gmail.com>
2019-04-06 19:14:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/vanguard', (request, response) => {
+app.get('/', (request, response) => {
     response.render('landing.hbs', {
         title: 'Vanguard Assault',
     });
