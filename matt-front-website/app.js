b34cc919702a8369d5956d24538a410d85982874
MatthewGlifonea <matthewglifonea@gmail.com>
2019-04-06 18:58:41 -0700

    html things

---
+++ matt-front-website/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const hbs = require('hbs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/vanguard', (request, response) => {
+    response.render('landing.hbs', {
+        title: 'Vanguard Assault',
+    });
+});
+app.get('/login', (request, response) => {
+    response.render('login.hbs', {
+        title: 'Login',
+    });
+});
+
+app.listen(8080, () => {
+    console.log('Vanguard Assult is online')
+});
+
