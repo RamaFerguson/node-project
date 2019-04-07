284d4515f536fbf905d35e7cbc13aa41f48225bf
Rama Ferguson <ferguson.rama@gmail.com>
2019-04-06 21:34:27 -0700

    added username parsing functionality for accounts.js

--- accounts.js
+++ accounts.js
@@ -1,13 +1,45 @@
 const fs = require('fs');
+const glob = require('glob');
 
 /** function that should be run when user submits username on login.
  * Parses all user json files in users directory for user with matching username
  * @param {username} username string
-*/
-function checkUserNameLogIn(username) {
+ */
+var checkUserNameLogIn = async function (inputUserName) {
 
-    pass
-}
+    // reads all json files in users directory
+    glob("users/*.json", function (error, files) {
+        if (error) {
+            console.log("Glob failed", err);
+        }
+        // reads each user file
+        files.forEach(function (file) {
+            // read current user file
+            fs.readFile(file, 'utf8', function (error, data) {
+                if (error) {
+                    console.log("Cannot read the file", error);
+                }
+                var userObject = JSON.parse(data);
+                // console.log(userObject);
+                if (userObject.username === inputUserName) {
+                    console.log('true');
+                    return true;
+                } else {
+                    console.log('false');
+                    return false;
+                }
+            });
+        });
+    });
+};
+
+//TODO: Stop searching JSONs once a match is made
+
+let gay = checkUserNameLogIn("darkflamemaster1983")
+// let gays = checkUserNameLogIn("helloFriend")
+// let gays2 = checkUserNameLogIn("hellofriend")
+
+
 
 module.exports = {
     checkUserNameLogIn
