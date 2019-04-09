d5e4dc6f2a19cb7d65b6e9b7226735e129ac43f6
Rama Ferguson <ferguson.rama@gmail.com>
2019-04-09 15:18:22 -0700

    Update database_utils.js

--- database_utils.js
+++ database_utils.js
@@ -30,6 +30,49 @@ function checkUserInDb(username, collection, database) {
     });
 }
 
+// function that checks if user exists in player database
+// returns True if they are
+// returns False if they do not exist
+function checkUserInDb(username, collection, database) {
+    return new Promise((resolve, reject) => {
+        // console.log(username);
+        // console.log(collection);
+        database.collection(collection).find({
+            "username": username
+        }, {
+            "username": 1
+        }).toArray(function (error, result) {
+            if (error) {
+                return reject(error);
+            } else {
+                if (result.length === 0) {
+                    resolve(false);
+                }
+                resolve(true);
+            }
+        });
+    });
+};
+
+function returnUserDetails(username, collection, database) {
+    return new Promise((resolve, reject) => {
+        // console.log(username);
+        // console.log(collection);
+        database.collection(collection).find({
+            "username": username
+        }).toArray(function (error, result) {
+            if (error) {
+                console.log('error in returnUserDetails')
+                reject(error);
+            } else {
+                console.log('returnUserDetails worked')
+                resolve(result);
+                //JSON.stringify(result.ops, undefined, 2)
+            };
+        });
+    });
+};
+
 var checkLiveGameAvailable = (player1, player2, collection, database) => {
     return new Promise((resolve, reject) => {
         let searchPlayers = [player1.uuid, player2.uuid].sort();
@@ -53,5 +96,6 @@ var checkLiveGameAvailable = (player1, player2, collection, database) => {
 
 module.exports = {
     checkUserInDb,
+    returnUserDetails,
     checkLiveGameAvailable
 };
