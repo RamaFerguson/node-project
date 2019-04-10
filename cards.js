286a81cbbff9429fff7f1856991522f75121f5d0
Nathan Chung <nchung77@gmail.com>
2019-04-09 19:46:30 -0700

    added renderGame function, cardKey to Card

--- cards.js
+++ cards.js
@@ -1,11 +1,10 @@
 module.exports = class Card {
-    constructor(cardInfo, index) {
+    constructor(cardInfo, cardKey) {
         this.name = cardInfo.name;
         this.attack = cardInfo.attack;
         this.health = cardInfo.health;
         this.cost = cardInfo.cost;
         this.keywords = cardInfo.keywords;
-
-        this.GID = index;
+        this.cardKey = cardKey;
     }
-}
+};
